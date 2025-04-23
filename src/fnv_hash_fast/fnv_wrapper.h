fd8954dbb1af56b32607148e2602ca5f32c8f4ca
J. Nick Koston <nick@koston.org>
2025-04-22 15:42:06 -1000

    chore: try multiply

--- src/fnv_hash_fast/fnv_wrapper.h
+++ src/fnv_hash_fast/fnv_wrapper.h
@@ -8,7 +8,7 @@ static inline uint32_t _c_fnv1a_32(const unsigned char *data, size_t len) {
     uint32_t hash = 0x811c9dc5;
     for (size_t i = 0; i < len; ++i) {
         hash ^= data[i];
-        hash += (hash<<1) + (hash<<4) + (hash<<7) + (hash<<8) + (hash<<24);
+        hash *= 0x01000193;
     }
     return hash;
 }
