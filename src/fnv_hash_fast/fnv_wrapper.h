d0cfcc8ecd8f439f61a54635cbd801625a40d0b1
J. Nick Koston <nick@koston.org>
2025-04-22 15:38:17 -1000

    feat: attempt to improve performance by avoid size calculation

--- src/fnv_hash_fast/fnv_wrapper.h
+++ src/fnv_hash_fast/fnv_wrapper.h
@@ -1,13 +1,13 @@
 
 
 #include <stdint.h>
+#include <stddef.h>
 
 // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
-uint32_t _c_fnv1a_32(const unsigned char *data) {
-    unsigned char *s = (unsigned char *)data;
-    uint32_t hash = ((uint32_t)0x811c9dc5);
-    while (*s) {
-        hash ^= (uint32_t)*s++;
+static inline uint32_t _c_fnv1a_32(const unsigned char *data, size_t len) {
+    uint32_t hash = 0x811c9dc5;
+    for (size_t i = 0; i < len; ++i) {
+        hash ^= data[i];
         hash += (hash<<1) + (hash<<4) + (hash<<7) + (hash<<8) + (hash<<24);
     }
     return hash;
