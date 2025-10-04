5df552387d3213e6a483911aacee13b18031a270
J. Nick Koston <nick@home-assistant.io>
2025-10-04 14:13:46 -0500

    feat: optimize binding and memoryview

--- src/fnv_hash_fast/_fnv_impl.pyx
+++ src/fnv_hash_fast/_fnv_impl.pyx
@@ -6,5 +6,6 @@ import cython
 cdef extern from "fnv_wrapper.h":
     unsigned int _c_fnv1a_32(const unsigned char *data, size_t len)
 
-def _fnv1a_32(data: bytes) -> int:
-    return _c_fnv1a_32(data, len(data))
+@cython.binding(False)
+def _fnv1a_32(const unsigned char[::1] data):
+    return _c_fnv1a_32(&data[0], data.shape[0])
