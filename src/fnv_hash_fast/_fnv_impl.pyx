b4cbb5fe5ebdde9fdb80316ecff7ed4b61f3c2e2
J. Nick Koston <nick@home-assistant.io>
2025-10-04 14:25:47 -0500

    fix: view overhead it wrose

--- src/fnv_hash_fast/_fnv_impl.pyx
+++ src/fnv_hash_fast/_fnv_impl.pyx
@@ -7,8 +7,5 @@ cdef extern from "fnv_wrapper.h":
     unsigned int _c_fnv1a_32(const unsigned char *data, size_t len)
 
 @cython.binding(False)
-def _fnv1a_32(const unsigned char[::1] data):
-    cdef size_t length = data.shape[0]
-    if length == 0:
-        return 0x811c9dc5  # FNV1a_32 offset basis for empty input
-    return _c_fnv1a_32(&data[0], length)
+def _fnv1a_32(data: bytes):
+    return _c_fnv1a_32(data, len(data))
